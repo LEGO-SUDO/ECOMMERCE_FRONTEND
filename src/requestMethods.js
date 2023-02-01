5b14fd8898063f78922ec8e774be243290abf02c
NAKUL  SINGH <nakulsingh@192.168.1.6>
2023-02-01 17:37:38 +0530

    second commit

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8080/api/'
+const BASE_URL = 'https://lyntra-api-k9nb.onrender.com/api/'
 const TOKEN =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZDdmYzBkZjA1YjM3MDkxOTliYzUwNyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NTA5OTE1NiwiZXhwIjoxNjc1MTg1NTU2fQ.xc_K2xDvNbe7OU2SWlLiIqDKUJfv0qQGvVJbmyDdnk0'
 
